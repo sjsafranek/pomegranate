c9be6af61a3ee5dd17415e203bc8ac659fef9ce2
stefan <stefan@stefan.stefan>
2017-01-12 16:17:42 -0800

    initial commit

--- pomegranate/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Zone
-from .models import Furniture
-
-admin.site.register(Zone)
-admin.site.register(Furniture)
--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^', include('pomegranate.urls')),
+	url(r'^', include('app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
