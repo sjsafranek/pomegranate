b3ec3d34464540909e0da6faa78191174549d4b0
Stefan Safranek <stefan@stefan>
2017-01-13 17:30:56 -0800

    database timeout

--- project/settings.py
+++ project/settings.py
@@ -78,7 +78,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+			'timeout': 20
+        }
         #'ATOMIC_REQUESTS': True
     }
 }
