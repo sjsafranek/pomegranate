2bec08ae6a9c300d967e5595e69a4eccdd27d445
stefan <stefan@stefan.stefan>
2017-01-19 18:50:25 -0800

    bootstrapper script edits

--- bootstrapper.sh
+++ bootstrapper.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-if [ -f "`pwd`/credentials.ini" ]; then
-	exit 1
-fi
-
-if [ ! -f "`pwd`/config.json" ]; then
-    echo '{"default":{"ENGINE":"django.contrib.gis.db.backends.spatialite","NAME":"geo.sqlite3","OPTIONS":{"timeout": 20}}}' >> config.json  
-fi
-
+# create file for logs
 if [ ! -d "`pwd`/log" ]; then
 	mkdir log
 fi
 
-# setup db
+# create app config 
+if [ ! -f "`pwd`/config.json" ]; then
+    echo '{"default":{"ENGINE":"django.contrib.gis.db.backends.spatialite","NAME":"geo.sqlite3","OPTIONS":{"timeout": 20}}}' >> config.json  
+fi
+
+# setup db and check for changes
 python3 manage.py makemigrations
 python3 manage.py migrate
 
 # create admin user
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@pomegranate.com', 'dev')" | python3 manage.py shell
-echo "[credentials]\nusername:admin\npassword:dev\n" >> credentials.ini
+if [ ! -f "`pwd`/credentials.ini" ]; then
+	echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@pomegranate.com', 'dev')" | python3 manage.py shell
+	echo "[credentials]\nusername:admin\npassword:dev\n" >> credentials.ini
+fi
+
