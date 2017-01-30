8965a7cc2fe8b6981f3961fa8cd6e8b1bdff8ccf
Stefan Safranek <stefan@stefan>
2017-01-30 12:54:27 -0800

    floorplan.geojson

--- data_processing/shp2geojson.sh
+++ data_processing/shp2geojson.sh
@@ -1,4 +1,5 @@
-ogr2ogr -f "ESRI Shapefile" liblines.shp wgs84.geojson -s_srs EPSG:4326 -t_srs EPSG:4326
+#ogr2ogr -f "ESRI Shapefile" liblines_proj2.shp wgs84.geojson -s_srs EPSG:4326 -t_srs EPSG:4326
 
+ogr2ogr -f "GeoJSON" wgs84.geojson liblines_proj2.shp -s_srs EPSG:4326 -t_srs EPSG:4269
 
-ogr2ogr -f "GeoJSON" wgs84.geojson liblines.shp -s_srs EPSG:4326 -t_srs EPSG:3857
+#ogr2ogr -f "GeoJSON" wgs84.geojson liblines_proj2.shp
